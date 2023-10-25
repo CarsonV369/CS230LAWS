51512c5d3110f3b3cef6fe1a3797a5772f57f153
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-10-24 20:59:39 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"></header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        "CS 230L"
+      </h1>
+      <h2>
+        "Section -003"
+      </h2>
+      <p>
+        "WVU ID 800283624"
+      </p>
+      <p>
+        "Hi I am Carson Verlinden"
+      </p>
     </div>
   );
 }
-
 export default App;
