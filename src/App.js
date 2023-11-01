e51b78e27a54157b3218925c08a8109f1ed710b2
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-10-31 23:25:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-<<<<<<< HEAD
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-=======
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
->>>>>>> 8990482aef329bfd25a389174fd9288343e3d8bf
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
